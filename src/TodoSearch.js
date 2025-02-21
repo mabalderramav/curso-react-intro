eca5bd816a8e9598313dfb4144088457496d889c
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2025-02-20 23:39:41 -0400

    Creates first react state like searchValue

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,10 +1,21 @@
+import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
+    const [searchValue, setSearchValue] = React.useState('');
+
+    console.log('Los usuarios buscan todos de ' + searchValue);
+
     return (
         <input
             placeholder="Cortar cebolla"
             className="TodoSearch"
+            value={searchValue}
+            onChange={
+                (event) => {
+                    setSearchValue(event.target.value);
+                }
+            }
         />
     );
 }
