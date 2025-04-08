e3139f8f159b55168067efeda10acdfaf2dfd86a
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2025-04-07 20:09:51 -0400

    use derivative states

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './TodoSearch.css';
 
-function TodoSearch() {
-    const [searchValue, setSearchValue] = React.useState('');
-
-    console.log('Los usuarios buscan todos de ' + searchValue);
+function TodoSearch({
+                        searchValue,
+                        setSearchValue,
+                    }) {
 
     return (
         <input
@@ -20,4 +20,4 @@ function TodoSearch() {
     );
 }
 
-export { TodoSearch };
+export {TodoSearch};
